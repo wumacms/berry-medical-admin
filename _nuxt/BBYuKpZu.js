dcbba6f434a20fa7e167211742f2261e3cf5064a
wumacms <wumacms@users.noreply.github.com>
2026-04-20 12:10:40 +0000

    deploy: bab839dd3a97ac78bce942a0761102e0af68c50d

---
+++ _nuxt/BBYuKpZu.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DlAUqK2U.js";import{o as r,c as t,M as n}from"./C8cOQIwM.js";const c={};function s(e,a){return r(),t("div",null,[n(e.$slots,"default")])}const _=o(c,[["render",s]]);export{_ as default};
